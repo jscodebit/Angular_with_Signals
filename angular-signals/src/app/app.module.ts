7d01bd61534e1c7d377f000574393a98969efe3a
jscodebit <soujanya.janapatla@gmail.com>
2026-03-10 22:55:17 -0600

    update angular version from 19 to 20

--- angular-signals/src/app/app.module.ts
+++ angular-signals/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
